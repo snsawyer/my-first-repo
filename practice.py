878d81705037d073e9d333d767b708f28423d83e
snsawyer <snsawyer@gmail.com>
2023-03-12 21:12:36 +0100

    This is my first code added.

---
+++ practice.py
@@ -0,0 +1,7 @@
+div_2 = [i for i in range(100,999) if i % 2 == 1]
+for i in div_2:
+    for test in [3,4,5,6,7]:
+        div_2 = [i for i in div_2 if i % test == 1]
+import math
+print(math.sqrt(5745))
+print(758**2)
